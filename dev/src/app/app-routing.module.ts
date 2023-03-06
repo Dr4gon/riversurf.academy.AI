6ab870cdcb8becf294a30e7ba5e9c7b685044556
Dr4gon <gaming@delong-iu.de>
2023-03-06 10:12:45 +0100

    Removed unnecessary injection because index.html already injects the component as root

--- dev/src/app/app-routing.module.ts
+++ dev/src/app/app-routing.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { RouteUrl } from './router-url';
-import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-    { path: RouteUrl.app, component: AppComponent },
+    { path: RouteUrl.app},
     // { path: '', redirectTo: RouteUrl.app, pathMatch: 'full' }
 ];
 

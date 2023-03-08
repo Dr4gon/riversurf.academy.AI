810f6b95dde2007313d6d982b1bf96266ef04a56
Dr4gon <gaming@delong-iu.de>
2023-03-08 14:59:12 +0100

    Added comment why sass files on componen level won't be used

--- dev/src/app/app.component.ts
+++ dev/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: require('./app.component.html').default, // https://stackoverflow.com/questions/59070216/webpack-file-loader-outputs-object-module
-  // styles: [ require('./app.component.sass').default ],
+  // styles: [ require('./app.component.sass').default ], -> Can't be used with global styles.sass because in the webpack configuration a raw-loader with esModule: false would disable the styles.sass transformation
 })
 export class AppComponent {
   title = 'dev';

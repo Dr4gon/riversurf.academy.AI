c31159a3b3f0c3c8218ebd097a62be78f08e1024
DR4GON\DeLong <gaming@delong-iu.de>
2023-07-26 17:32:54 +0200

    Update format options for linting and prettier

--- dev/.eslintrc.cjs
+++ dev/.eslintrc.cjs
@@ -3,22 +3,41 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  overrides: [
-    {
-      files: [
-        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:cypress/recommended'
-      ]
-    }
-  ],
+  globals: {
+    process: true,
+  },
+  env: {
+    node: true,
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    ecmaVersion: 'latest',
+  },
+  extends: ['airbnb-base', 'plugin:prettier/recommended', 'prettier'],
+  plugins: ['import', 'prettier'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'import/newline-after-import': ['error', { count: 1 }],
+    'no-unused-vars': ['warn'],
+    'prefer-template': ['warn'],
+    'class-methods-use-this': ['warn', { exceptMethods: ['profile'], enforceForClassFields: true }],
+    'func-names': ['warn', 'as-needed'],
+    'prefer-const': ['warn'],
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        arrowParens: 'avoid',
+        printWidth: 120,
+        semi: false,
+        trailingComma: 'es5',
+        tabWidth: 2,
+        useTabs: false,
+      },
+    ],
+  },
 }
+

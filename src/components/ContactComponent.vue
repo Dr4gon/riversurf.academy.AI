91b33d826dce9b0dffca294dcfe939e73c60bd59
mhmFrost <tim.thieler@brightstraining.com>
2023-07-31 21:24:23 +0200

    Add form fields to ContactComponent

--- src/components/ContactComponent.vue
+++ src/components/ContactComponent.vue
@@ -3,7 +3,17 @@
 </script>
 
 <template>
-<p>Try to Contact De Long here.</p>
+  <form>
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name" required>
+
+    <label for="message">Message:</label>
+    <textarea id="message" name="message" rows="5"
+              placeholder="Was willst du mit mir besprechen?"
+              style="width: 300px; height: 150px; resize: none" required></textarea>
+
+    <input type="submit" value="Submit">
+  </form>
 </template>
 
 <style scoped>

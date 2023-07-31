5db4e6a1aa4f19f7892afc9cfbb44eba40f244bf
Dr4gon <gaming@delong-iu.de>
2023-07-31 21:22:18 +0200

    Secure resource loading with https

--- src/components/WelcomeComponent.vue
+++ src/components/WelcomeComponent.vue
@@ -7,7 +7,7 @@
     <div>
       <h1>Welcome to Riversurf Academy 🤙</h1>
       <video width="640" height="360" controls>
-        <source src="http://www.riversurf.academy/WillkommensVideo_Edit_sub.mp4" type="video/mp4">
+        <source src="https://www.riversurf.academy/WillkommensVideo_Edit_sub.mp4" type="video/mp4">
         Your browser does not support the video tag.
       </video>
     </div>

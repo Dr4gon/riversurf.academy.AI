2ffa8eab38035e2240d1895ace3cd5d357a67023
Dr4gon <gaming@delong-iu.de>
2023-07-31 19:06:08 +0200

    Add basic navigaton for components until slider navigation is implemented

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import WelcomeComponent from "@/components/WelcomeComponent.vue";
-import ConvincerComponent from "@/components/ConvincerComponent.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-
+    {
+      path: '/welcome',
+      name: 'welcome',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../components/WelcomeComponent.vue')
+    },
+    {
+      path: '/convincer',
+      name: 'convincer',
+      component: () => import('../components/ConvincerComponent.vue')
+    }
   ]
 })
 

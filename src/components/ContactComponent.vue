c98ea867a94b3808984f45a7f29556badc86188e
mhmFrost <tim.thieler@brightstraining.com>
2023-08-04 10:43:43 +0200

    Add validation for user input in ContactComponent.vue

--- src/components/ContactComponent.vue
+++ src/components/ContactComponent.vue
@@ -4,16 +4,48 @@ import {computed, ref} from 'vue';
 const isSubmitted = ref(false);
 const isFast = ref(false);
 const name = ref('');
+const email = ref('')
+const info = ref(''); //Honeypot
 const message = ref('');
 const startTime = ref(0);
 const endTime = ref(0);
-const info = ref(''); //Honeypot
+
 const submitMessageRight = computed(() => `Danke für deine Nachricht ${name.value}, ich melde mich bei dir 🤙`)
 const submitMessageFalse = computed(() => `Hier ist etwas schief gelaufen ${name.value}, bitte probiere es erneut.`)
 
 const setStartTime = () => {
   if (startTime.value === 0) {
     startTime.value = Date.now();
+  }
+};
+
+const handleNameInput = () => {
+  setStartTime();
+  const nameElem = document.getElementById('name');
+  if (name.value.trim() === '') {
+    nameElem && nameElem.setCustomValidity('Bitte geben Sie einen Namen ein.🤨');
+  } else {
+    nameElem && nameElem.setCustomValidity('');
+  }
+};
+
+const handleEmailInput = () => {
+  setStartTime();
+  const emailElem = document.getElementById('email');
+  if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(email.value)) {
+    emailElem && emailElem.setCustomValidity('Bitte geben Sie eine gültige E-Mail-Adresse ein. 🤨');
+  } else {
+    emailElem && emailElem.setCustomValidity('');
+  }
+};
+
+const handleMessageInput = () => {
+  setStartTime();
+  const messageElem = document.getElementById('message');
+  if (message.value.trim() === '') {
+    messageElem && messageElem.setCustomValidity('Bitte geben Sie eine Nachricht ein. 🤨');
+  } else {
+    messageElem && messageElem.setCustomValidity('');
   }
 };
 
@@ -31,6 +63,7 @@ const closeMessage = () => {
   isFast.value = false;
   name.value = '';
   message.value = '';
+  email.value = '';
   startTime.value = 0;
   endTime.value = 0;
 };
@@ -40,24 +73,30 @@ const closeMessage = () => {
   <form id="contact-form" @submit.prevent="handleSubmit">
 
     <label for="name">Name:</label>
-    <input type="text" id="name" v-model="name" @input="setStartTime" placeholder="Name" required>
+    <input type="text" id="name" v-model="name" @input="handleNameInput" placeholder="Name">
+
+    <label for="email">Email:</label>
+    <input type="email" id="email" v-model="email" @input="handleEmailInput" placeholder="email@mail.com">
 
     <input type="text" id="info" v-model="info" style="display: none;">
 
     <label for="message">Message:</label>
-
-    <textarea id="message" v-model="message" @input="setStartTime" name="message" rows="5"
+    <textarea id="message" v-model="message" @input="handleMessageInput" name="message" rows="5"
               placeholder="Was willst du mit mir besprechen?"
-              style="width: 300px; height: 150px; resize: none" required></textarea>
+              style="width: 300px; height: 150px; resize: none"></textarea>
+
     <input type="submit" value="Submit">
+
     <div v-if="isSubmitted" class="submit-message">
       <span @click="closeMessage" class="close-btn">X</span>
       <p class="submitMessageRight">{{ submitMessageRight }}</p>
     </div>
+
     <div v-if="isFast" class="submit-message">
       <span @click="closeMessage" class="close-btn">X</span>
       <p class="submitMessageFalse">{{ submitMessageFalse }}</p>
     </div>
+
   </form>
 </template>
 
--- src/components/__tests__/SpamProtectionContact.spec.js
+++ src/components/__tests__/SpamProtectionContact.spec.js
@@ -3,14 +3,16 @@ import { mount } from '@vue/test-utils';
 import ContactComponent from "@/components/ContactComponent.vue";
 
 
+
 function delay(time) {
   return new Promise(resolve => setTimeout(resolve, time));
 }
 
-async function fillAndSubmitForm(testName, wrapper, name, message, wait = 0) {
+async function fillAndSubmitForm(testName, wrapper, name, email, message, wait = 0) {
   const nameInput = wrapper.find('#name');
-  const messageInput = wrapper.find('#message');
+  const emailInput = wrapper.find('#email')
   const infoInput = wrapper.find('#info');
+  const messageInput = wrapper.find('#message');
   const submitButton = wrapper.find('input[type="submit"]');
 
   // Wait for the form to be visible and interactive
@@ -18,6 +20,7 @@ async function fillAndSubmitForm(testName, wrapper, name, message, wait = 0) {
 
   // Set values for the form inputs
   await nameInput.setValue(name);
+  await emailInput.setValue(email);
   await messageInput.setValue(message);
 
   // Set value for the honeypot input if test name contains 'honeypot'
@@ -37,24 +40,24 @@ async function fillAndSubmitForm(testName, wrapper, name, message, wait = 0) {
 
 test('Submit contact form quickly', async () => {
   const wrapper = mount(ContactComponent);
-  await fillAndSubmitForm('Submit contact form quickly', wrapper, 'John Doe', 'This is a test message');
+  await fillAndSubmitForm('Submit contact form quickly', wrapper, 'John Doe', 'testingemail@mail.com', 'This is a test message');
   expect(wrapper.html()).toContain('submitMessageFalse');
 });
 
 test('Submit contact form after delay', async () => {
   const wrapper = mount(ContactComponent);
-  await fillAndSubmitForm('Submit contact form after delay', wrapper, 'John Doe', 'This is a test message', 4100);
+  await fillAndSubmitForm('Submit contact form after delay', wrapper, 'John Doe', 'testingemail@mail.com', 'This is a test message', 4100);
   expect(wrapper.html()).toContain('submitMessageRight');
 });
 
 test('Submit contact form with honeypot after delay', async () => {
   const wrapper = mount(ContactComponent);
-  await fillAndSubmitForm('Submit contact form with honeypot after delay', wrapper, 'John Doe', 'This is a test message', 4100);
+  await fillAndSubmitForm('Submit contact form with honeypot after delay', wrapper, 'John Doe', 'testingemail@mail.com', 'This is a test message', 4100);
   expect(wrapper.html()).toContain('submitMessageFalse');
 });
 
 test('Submit contact form with honeypot quickly', async () => {
   const wrapper = mount(ContactComponent);
-  await fillAndSubmitForm('Submit contact form with honeypot quickly', wrapper, 'John Doe', 'This is a test message');
+  await fillAndSubmitForm('Submit contact form with honeypot quickly', wrapper, 'John Doe', 'testingemail@mail.com', 'This is a test message');
   expect(wrapper.html()).toContain('submitMessageFalse');
 });

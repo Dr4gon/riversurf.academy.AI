65ce9696f467de046815784adb9af4d566b318e1
mhmFrost <tim.thieler@brightstraining.com>
2023-08-07 14:50:14 +0200

    Created temporary design for SalesComponent.vue and implement ProductComponent

--- src/components/SalesComponent.vue
+++ src/components/SalesComponent.vue
@@ -1,11 +1,36 @@
 <script setup>
-
+import ProductComponent from "@/components/ProductComponent.vue";
 </script>
 
 <template>
-<p>Produktpalette</p>
+  <!-- Überschrift mittig ausrichten -->
+  <div class="header-container">
+    <h1>De Longs, 3 favorites 🤙</h1>
+  </div>
+
+  <!-- Container für die 3 Divs nebeneinander -->
+  <div class="products-container">
+    <div class="product"><ProductComponent /></div>
+    <div class="product"><ProductComponent /></div>
+    <div class="product"><ProductComponent /></div>
+  </div>
 </template>
 
 <style scoped>
+.header-container {
+  text-align: center;
+  margin-bottom: 20px; /* Abstand unterhalb der Überschrift */
+}
 
+.products-container {
+  display: flex; /* Flexbox verwenden, um die Divs nebeneinander anzuzeigen */
+  justify-content: space-between; /* Gleichmäßiger Abstand zwischen den Divs */
+}
+
+.product {
+  flex: 1; /* Jedes Produkt nimmt den gleichen Platz ein */
+  margin: 0 10px; /* Ein kleiner Abstand zwischen den Produkten */
+}
+
+/* Hier können Sie zusätzliche Styling-Einstellungen für die ProductComponent vornehmen */
 </style>

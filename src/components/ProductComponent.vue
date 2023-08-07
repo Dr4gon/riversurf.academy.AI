ffc0465beebe036ee34078236ac75603887cb64d
mhmFrost <tim.thieler@brightstraining.com>
2023-08-07 14:49:16 +0200

    Added ProductComponent and created temporary design

---
+++ src/components/ProductComponent.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="product">
+    <h3>Just Relax!</h3>
+    <hr>
+    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+      dolore
+      magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+      gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+      sadipscing
+      elitr. </p>
+
+    <img src="@/assets/justrelax.jpg" alt="Just Relax">
+    <br>
+    <hr>
+    <button>Purchase / Order now</button>
+  </div>
+</template>
+
+<style scoped>
+.product{
+  text-align: center;
+}
+</style>

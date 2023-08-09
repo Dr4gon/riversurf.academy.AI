a113d176dd2bb35d79e23f12a0391fd6f1aff86a
mhmFrost <tim.thieler@brightstraining.com>
2023-08-09 10:30:52 +0200

    Added gradient Backgrounds to ContactComponent.vue and AboutComponent.vue, under water feeling

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -3,9 +3,18 @@
 </script>
 
 <template>
+  <div class="temp">
 <p>It's a meee, Mario</p>
+  </div>
 </template>
 
 <style scoped>
-
+.temp{
+  background: rgb(13,20,23);
+  background: linear-gradient(0deg, rgba(13,20,23,1) 9%, rgba(62,96,111,1) 69%);
+  height: 100vh;
+}
+p{
+  margin: 0; /* Margin ist woher auch immer auf 16px eingestellt und erstellt unschönen weißen rand zwischen den components */
+}
 </style>

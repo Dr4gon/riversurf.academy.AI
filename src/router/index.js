fccae951a992c0b75183da12b922525f2de31bff
Dr4gon <gaming@delong-iu.de>
2023-08-17 15:58:31 +0200

    Prettify index.js

--- src/router/index.js
+++ src/router/index.js
@@ -11,27 +11,27 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* @vite-ignore */ COMPONENT_PATH + 'WelcomeComponent.vue'),
+      component: () => import(/* @vite-ignore */ `${COMPONENT_PATH}WelcomeComponent.vue`),
     },
     {
       path: '/convincer',
       name: 'convincer',
-      component: () => import(/* @vite-ignore */ COMPONENT_PATH + 'ConvincerComponent.vue'),
+      component: () => import(/* @vite-ignore */ `${COMPONENT_PATH}ConvincerComponent.vue`),
     },
     {
       path: '/contact',
       name: 'contact',
-      component: () => import(/* @vite-ignore */ COMPONENT_PATH + 'ContactComponent.vue'),
+      component: () => import(/* @vite-ignore */ `${COMPONENT_PATH}ContactComponent.vue`),
     },
     {
       path: '/sales',
       name: 'sales',
-      component: () => import(/* @vite-ignore */ COMPONENT_PATH + 'SalesComponent.vue'),
+      component: () => import(/* @vite-ignore */ `${COMPONENT_PATH}SalesComponent.vue`),
     },
     {
       path: '/about',
       name: 'about',
-      component: () => import(/* @vite-ignore */ COMPONENT_PATH + 'AboutComponent.vue'),
+      component: () => import(/* @vite-ignore */ `${COMPONENT_PATH}AboutComponent.vue`),
     },
   ],
 });

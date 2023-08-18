4b6f1bf596bc14334868cec012ca0e0bb3fca3b0
Dr4gon <gaming@delong-iu.de>
2023-08-18 11:42:45 +0200

    Apply new video style to about section as well

--- src/components/WelcomeComponent.vue
+++ src/components/WelcomeComponent.vue
@@ -2,19 +2,11 @@
 
 <template>
   <div class="container">
-    <div>
-      <video class="video" controls>
-        <source src="https://www.riversurf.academy/WillkommensVideo_Edit_sub.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-    </div>
+    <video controls>
+      <source src="https://www.riversurf.academy/WillkommensVideo_Edit_sub.mp4" type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
   </div>
 </template>
 
-<style scoped>
-.video {
-  width: 78vw;
-  /* height: 75vh; Let the video decide upon the width */
-  padding: 5vh;
-}
-</style>
+<style scoped></style>

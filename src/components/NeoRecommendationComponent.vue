8cf918a6991381a83a7fe30063ca638db49d593a
mhmFrost <tim.thieler@brightstraining.com>
2023-11-03 13:47:45 +0100

    Implemented a new component that dynamically fetches and provides data from the generator based on user input.

---
+++ src/components/NeoRecommendationComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <p>Wassertemperatur: {{ waterTemperature }}</p>
+    <p>Geschlecht: {{ gender }}</p>
+    <p>Level: {{ level }}</p>
+    <p>Budget: {{ budget }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    waterTemperature: {
+      type: String,
+      required: true
+    },
+    gender: {
+      type: String,
+      required: true
+    },
+    level: {
+      type: String,
+      required: true
+    },
+    budget: {
+      type: String,
+      required: true
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/NeoSelectorView.vue
+++ src/views/NeoSelectorView.vue
@@ -1,22 +1,21 @@
 <template>
   <div class='container'>
     <div class='view neo'>
-      <transition-group name="slide-up">
+      <transition-group name='slide-up'>
         <div class='generator' v-if='!submitted'>
           <h1>Neopren Generator</h1>
-          <p>Du suchst nach einem passenden Neopren Anzug für dein nächstes Riversurf Abenteuer? Was für einen Neopren
-            Anzug
-            du brauchst und auf was du beim Kauf achten solltest, erfährst du hier in.</p>
+          <p>Welchen Neo du brauchst sagt dir unser Neo Generator. Fülle einfach die Felder aus und drücke auf
+            Start.</p>
           <label>
             Welche Wassertemperatur hat dein Zielgewässer?
             <select v-model='waterTemperature'>
               <option disabled value=''>Bitte auswählen</option>
-              <option value='above23'>über 23°</option>
-              <option value='between19and23'>19° - 23°</option>
-              <option value='between15and20'>15° - 20°</option>
-              <option value='between12and17'>12° - 17°</option>
-              <option value='between9and13'>9° - 13°</option>
-              <option value='under9'>unter 9°</option>
+              <option value='Above23Degree'>über 23°</option>
+              <option value='between19and23Degree'>19° - 23°</option>
+              <option value='between15and20Degree'>15° - 20°</option>
+              <option value='between12and17Degree'>12° - 17°</option>
+              <option value='between9and13Degree'>9° - 13°</option>
+              <option value='under9Degree'>unter 9°</option>
             </select>
           </label>
           <br>
@@ -24,23 +23,41 @@
             Wie ist dein Geschlecht?
             <select v-model='gender'>
               <option disabled value=''>Bitte auswählen</option>
-              <option value='m'>Männlich</option>
-              <option value='f'>Weiblich</option>
+              <option value='Male'>Männlich</option>
+              <option value='Female'>Weiblich</option>
+            </select>
+          </label>
+          <br>
+          <label>
+            Wie ist dein Level?
+            <select v-model='level'>
+              <option disabled value=''>Bitte auswählen</option>
+              <option value='Beginner'>Anfänger</option>
+              <option value='Intermediate'>Hobby</option>
+              <option value='Professional'>Professional</option>
+            </select>
+          </label>
+          <br>
+          <label>
+            Wie hoch ist dein Budget?
+            <select v-model='budget'>
+              <option disabled value=''>Bitte auswählen</option>
+              <option value='LowBudget'>Kleines Budget</option>
+              <option value='MidBudget'>Mittleres Budget</option>
+              <option value='HighBudget'>Großes Budget</option>
             </select>
           </label>
           <br>
           <button @click='submit'>Start</button>
         </div>
         <div class='generatedContent' v-if='submitted'>
-          <MaleAbove23 v-if="waterTemperature === 'above23' && gender ==='m'" />
-          <FemaleAbove23 v-if="waterTemperature ==='above23' && gender==='f'" />
-          <MaleBetween19and23 v-if="waterTemperature === 'between19and23' && gender ==='m'" />
-          <FemaleBetween19and23 v-if="waterTemperature ==='between19and23' && gender==='f'" />
-          <Between15and20 v-if="waterTemperature === 'between15and20'" />
-          <Between12and17 v-if="waterTemperature === 'between12and17'" />
-          <Between9and13 v-if="waterTemperature === 'between9and13'" />
-          <Under9 v-if="waterTemperature === 'under9'" />
-          <button @click='submitted = false'>Zurück</button>
+          <NeoRecommendationComponent
+            :waterTemperature="waterTemperature"
+            :gender="gender"
+            :level="level"
+            :budget="budget"
+          />
+            <button @click='submitted = false'>Zurück</button>
         </div>
       </transition-group>
     </div>
@@ -50,40 +67,30 @@
 
 <script>
 
-import MaleAbove23 from '../components/neo/MaleAbove23.vue';
-import FemaleAbove23 from '../components/neo/FemaleAbove23.vue';
-import MaleBetween19and23 from '../components/neo/MaleBetween19and23.vue';
-import FemaleBetween19and23 from '../components/neo/FemaleBetween19and23.vue';
-import Between15and20 from '../components/neo/Between15and20.vue';
-import Between12and17 from '../components/neo/Between12and17.vue';
-import Between9and13 from '../components/neo/Between9and13.vue';
-import Under9 from '../components/neo/Under9.vue';
+import NeoRecommendationComponent from '../components/NeoRecommendationComponent.vue';
+
 
 export default {
   components: {
-    MaleAbove23,
-    FemaleAbove23,
-    MaleBetween19and23,
-    FemaleBetween19and23,
-    Between15and20,
-    Between12and17,
-    Between9and13,
-    Under9,
+    NeoRecommendationComponent,
+
   },
 
   data() {
     return {
       waterTemperature: '',
       gender: '',
+      budget: '',
+      level: '',
       submitted: false,
     };
   },
   methods: {
     submit() {
-      if (this.waterTemperature && this.gender) {
+      if (this.waterTemperature && this.gender && this.budget && this.level) {
         this.submitted = true;
       } else {
-        alert('Bitte füllen Sie beide Felder aus.');
+        alert('Bitte füllen Sie alle Felder aus.');
       }
     },
   },
@@ -105,12 +112,13 @@ export default {
 
 .slide-up-enter-from {
   opacity: 0;
-  transform: translateY(300px);
+  transform: translateY(200px);
 }
 
 .slide-up-leave-to {
   opacity: 0;
-  transform: translateY(-300px);
+  transform: translateY(-200px);
+  position: absolute;
 }
 
 </style>

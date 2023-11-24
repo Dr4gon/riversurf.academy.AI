9eba71dde4ea189145a54ed13b405ce52ddac7e9
DR4GON\DeLong <gaming@delong-iu.de>
2023-11-24 08:52:41 +0100

    Apply styling separately to actual changes

--- src/views/NeoSelectorView.vue
+++ src/views/NeoSelectorView.vue
@@ -1,79 +1,76 @@
 <template>
-  <div class='container'>
-    <div class='view neo'>
-      <transition-group name='slide-up'>
-        <div class='generator' v-if='!submitted'>
+  <div class="container">
+    <div class="view neo">
+      <transition-group name="slide-up">
+        <div class="generator" v-if="!submitted">
           <h1>Neopren Generator</h1>
-          <p>Welchen Neo du brauchst sagt dir unser Neo Generator. Fülle einfach die Felder aus und drücke auf
-            Start.</p>
+          <p>
+            Welchen Neo du brauchst sagt dir unser Neo Generator. Fülle einfach die Felder aus und drücke auf Start.
+          </p>
           <label>
             Welche Wassertemperatur hat dein Zielgewässer?
-            <select v-model='waterTemperature'>
-              <option disabled value=''>Bitte auswählen</option>
-              <option value='Above23Degree'>über 23°</option>
-              <option value='between19and23Degree'>19° - 23°</option>
-              <option value='between15and20Degree'>15° - 20°</option>
-              <option value='between12and17Degree'>12° - 17°</option>
-              <option value='between9and13Degree'>9° - 13°</option>
-              <option value='under9Degree'>unter 9°</option>
+            <select v-model="waterTemperature">
+              <option disabled value="">Bitte auswählen</option>
+              <option value="Above23Degree">über 23°</option>
+              <option value="between19and23Degree">19° - 23°</option>
+              <option value="between15and20Degree">15° - 20°</option>
+              <option value="between12and17Degree">12° - 17°</option>
+              <option value="between9and13Degree">9° - 13°</option>
+              <option value="under9Degree">unter 9°</option>
             </select>
           </label>
-          <br>
+          <br />
           <label>
             Wie ist dein Geschlecht?
-            <select v-model='gender'>
-              <option disabled value=''>Bitte auswählen</option>
-              <option value='Male'>Männlich</option>
-              <option value='Female'>Weiblich</option>
+            <select v-model="gender">
+              <option disabled value="">Bitte auswählen</option>
+              <option value="Male">Männlich</option>
+              <option value="Female">Weiblich</option>
             </select>
           </label>
-          <br>
+          <br />
           <label>
             Wie ist dein Level?
-            <select v-model='level'>
-              <option disabled value=''>Bitte auswählen</option>
-              <option value='Beginner'>Anfänger</option>
-              <option value='Intermediate'>Hobby</option>
-              <option value='Professional'>Professional</option>
+            <select v-model="level">
+              <option disabled value="">Bitte auswählen</option>
+              <option value="Beginner">Anfänger</option>
+              <option value="Intermediate">Hobby</option>
+              <option value="Professional">Professional</option>
             </select>
           </label>
-          <br>
+          <br />
           <label>
             Wie hoch ist dein Budget?
-            <select v-model='budget'>
-              <option disabled value=''>Bitte auswählen</option>
-              <option value='LowBudget'>Kleines Budget</option>
-              <option value='MidBudget'>Mittleres Budget</option>
-              <option value='HighBudget'>Großes Budget</option>
+            <select v-model="budget">
+              <option disabled value="">Bitte auswählen</option>
+              <option value="LowBudget">Kleines Budget</option>
+              <option value="MidBudget">Mittleres Budget</option>
+              <option value="HighBudget">Großes Budget</option>
             </select>
           </label>
-          <br>
-          <button @click='submit'>Start</button>
+          <br />
+          <button @click="submit">Start</button>
         </div>
-        <div class='generatedContent' v-if='submitted'>
+        <div class="generatedContent" v-if="submitted">
           <NeoRecommendationComponent
             :waterTemperature="waterTemperature"
             :gender="gender"
             :level="level"
             :budget="budget"
           />
-            <button @click='submitted = false'>Zurück</button>
+          <button @click="submitted = false">Zurück</button>
         </div>
       </transition-group>
     </div>
-
   </div>
 </template>
 
 <script>
-
 import NeoRecommendationComponent from '../components/NeoRecommendationComponent.vue';
-
 
 export default {
   components: {
     NeoRecommendationComponent,
-
   },
 
   data() {
@@ -120,5 +117,4 @@ export default {
   transform: translateY(-200px);
   position: absolute;
 }
-
 </style>

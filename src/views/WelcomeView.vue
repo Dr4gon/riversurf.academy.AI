15519b8b4009e5f6d1d11bdc291ca56af2cb9a32
DR4GON\DeLong <gaming@delong-iu.de>
2024-01-03 16:00:41 +0100

    Simplify initial entry point for user with 2 calls to action

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,14 +1,37 @@
 <template>
   <div class="container">
     <div class="view">
-      <video controls>
-        <source src="https://www.riversurf.academy/WillkommensVideo_Edit_sub.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      <p>
+        <input type="text" placeholder="What's your question about riversurfing?" />
+        <button>Ask</button>
+      </p>
+
+      <p>
+        <label class="center">OR</label>
+      </p>
+
+      <p>
+        <label>Upload a video for feedback</label>
+        <input type="file" accept="video/*" />
+        <button>Upload</button>
+      </p>
     </div>
   </div>
 </template>
 
 <script setup></script>
 
-<style scoped></style>
+<style scoped>
+input {
+  width: 55vw;
+}
+
+p {
+  margin-left: 6vw;
+  margin-right: 6vw;
+}
+
+.center {
+  margin-left: 30vw;
+}
+</style>

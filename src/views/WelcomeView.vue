7e5f6bb07676566ffd43f369c9e1c3b35a516fcf
mhmFrost <tim.thieler@brightstraining.com>
2024-01-23 12:34:16 +0100

    Integrated UUID generation and storage in Local Storage for user tracking

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="container">
-    <div class="view">
-      <form >
-        <input class="text" v-model="userQuestion" placeholder="What's your question about riversurfing?" />
-        <button type="submit" @click.prevent="askChatGPT">Ask</button>
+  <div class='container'>
+    <div class='view'>
+      <form>
+        <input class='text' v-model='userQuestion' placeholder="What's your question about riversurfing?" />
+        <button type='submit' @click.prevent='askChatGPT'>Ask</button>
       </form>
       <h3>{{ answer }}</h3>
       <p>
-        <label class="center">OR</label>
+        <label class='center'>OR</label>
       </p>
 
       <p>
         <label>Upload a video for feedback</label>
-        <input type="file" accept="video/*" />
+        <input type='file' accept='video/*' />
         <button>Upload</button>
       </p>
     </div>
@@ -27,13 +27,27 @@ export default {
   data() {
     return {
       userQuestion: '',
-      answer: ''
+      answer: '',
     };
   },
+  created() {
+    this.checkAndSetUUID();
+  },
   methods: {
+    checkAndSetUUID() {
+      if (!localStorage.getItem('userIdentifier')) {
+        localStorage.setItem('userIdentifier', this.generateUUID());
+      }
+    },
+    generateUUID() {
+      return 'xxxxxxxx-xxxx-xxxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        const r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+      });
+    },
     async askChatGPT() {
       try {
-        const response = await axios.post('http://localhost:3000/api/request', { text: this.userQuestion });
+        const response = await axios.post('http://localhost:3000/api/request', { text: this.userQuestion, uuid: uuid });
         this.answer = response.data.reply;
       } catch (error) {
         console.error('Fehler beim Senden der Anfrage:', error);

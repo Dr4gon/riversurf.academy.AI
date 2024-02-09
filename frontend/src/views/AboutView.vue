208564022b3f0856d78052b140ce78ba9f84bf9a
DR4GON\DeLong <gaming@delong-iu.de>
2024-02-09 20:44:52 +0100

    Remove out dated video

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="container about-background">
-    <div class="view">
-      <video controls>
-        <source src="https://www.riversurf.academy/WillkommensVideo_Edit_sub.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-    </div>
+    <div class="view"></div>
   </div>
 </template>
 

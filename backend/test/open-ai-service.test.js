f35cc58bd700d77c5fd9ca3c26241c1aac6285d5
DR4GON\DeLong <gaming@delong-iu.de>
2024-03-11 19:42:06 +0100

    Improve test conditions with simple statistical re-runs

--- backend/test/open-ai-service.test.js
+++ backend/test/open-ai-service.test.js
@@ -8,12 +8,20 @@ test(`Send chat message and verify its storage`, async () => {
 });
 
 test('Beginner asks for river wave to start in Munich and answer must include Floßlände', async () => {
-  const response = await request(app)
-    .post('/api/openai')
-    .send({ text: 'Wo kann ich in München das riversurfen anfangen?', uuid: 'Test-UUID' })
-    .expect(200);
-  expect(response.body.reply).toContain('Floßlände');
-});
+  let numberOfSuccessfulResponses = 0;
+
+  for (let i = 0; i < 10; i++) {
+    const response = await request(app)
+      .post('/api/openai')
+      .send({ text: 'Wo kann ich in München das riversurfen anfangen?', uuid: 'Test-UUID' })
+      .expect(200);
+    if (response.body.reply.includes('Floßlände')) {
+      numberOfSuccessfulResponses++;
+    }
+  }
+
+  expect(numberOfSuccessfulResponses).toBeGreaterThan(7);
+}, 30000);
 
 afterEach(async () => {
   await request(app).delete('/admin/conversation/Test-UUID').expect(200);

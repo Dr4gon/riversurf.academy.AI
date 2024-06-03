10a9c555ee32ee983dfb629f84f4804885fc058d
DR4GON\DeLong <gaming@delong-iu.de>
2024-06-03 21:42:09 +0200

    Connect video upload to external s3 bucket

--- backend/src/routes/upload-service.js
+++ backend/src/routes/upload-service.js
@@ -1,6 +1,33 @@
 const express = require('express');
+
+const { S3Client } = require('@aws-sdk/client-s3');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const multerS3 = require('multer-s3');
+
+let s3 = new S3Client({
+  region: process.env.REGION,
+  credentials: {
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  },
+  sslEnabled: false,
+  s3ForcePathStyle: true,
+  signatureVersion: 'v4',
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.BUCKET_NAME,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString() + '-' + file.originalname);
+    },
+  }),
+});
 
 const router = express.Router();
 

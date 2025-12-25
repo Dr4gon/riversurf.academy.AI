84679280b9527381039a979773cce4351e27df61
De Long <di@zammad.com>
2025-12-26 00:11:57 +0100

    Configure Reverse Proxy host IP to allow access to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,6 +12,7 @@ const UploadService = require('./routes/upload-service');
 
 const app = express();
 const port = 3000;
+const reverseProxyHost= '0.0.0.0';
 
 app.use(
   helmet({
@@ -38,7 +39,7 @@ app.use(cors());
 const start = async () => {
   try {
     await mongoose.connect(`${process.env.MONGODB}`);
-    app.listen(3000, () => console.log(`Server started on port ${port}`));
+    app.listen(port, reverseProxyHost, () => console.log(`Server started with ip ${reverseProxyHost} on port ${port}`));
   } catch (error) {
     console.error(error);
   }
